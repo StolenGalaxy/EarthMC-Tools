fe656cf0b5082e0256b36daa0fa3af9fc85d148c
Matthew Ridley <56508206+StolenGalaxy@users.noreply.github.com>
2026-01-30 18:58:03 +0000

    Update run.py

--- run.py
+++ run.py
@@ -1,21 +1,36 @@
 from hunting import Hunter
-
 from tools import Calculator, Data
 
-hunter = Hunter()
+
+# Preferences
+self_ign = ""
+player_data_refresh_delay = 15
+base_data_refresh_delay = 600
+
 
 calculator = Calculator()
+data_finder = Data(self_ign)
 
-data_finder = Data()
 
-running = False
-while not running:
-    mode = input("""Select mode\nHunting -> 1\nPlayer Finder -> 2\nEnter choice: """)
+def select_mode():
+    running = False
+    while not running:
+        mode = input("""Select mode\nHunting -> 1\nPlayer Finder -> 2\nEnter choice: """)
 
-    if mode == 1:
-        running = True
-        hunter.run(calculator, data_finder)
-    elif mode == 2:
-        running = True
-        # Here for future implementation
-        # player_finder.run(calculator, data_finder)
+        if mode == 1:
+            hunter = Hunter(calculator, data_finder, player_data_refresh_delay, base_data_refresh_delay)
+            running = True
+            return hunter
+
+        elif mode == 2:
+            running = True
+            # (future implementation)
+
+
+def main():
+    mode = select_mode()
+    mode.run()
+
+
+if __name__ == "__main__":
+    main()
